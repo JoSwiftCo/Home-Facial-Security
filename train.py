08732a1277a45dcf157f61ff3feb09bf1d4ab6e9
Kien Ngo <kienn@aareas.com>
2022-05-20 09:18:55 -0500

    Fix typo in train.py

--- train.py
+++ train.py
@@ -28,5 +28,5 @@ faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
 # Save the model into trainer/trainer.yml
 recognizer.write('/home/pi/Desktop/Face_recognition/trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
-# Print the numer of faces trained and end program
+# Print the number of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
